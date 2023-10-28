08b6923f0b5434da4e85090007f9a8f376a4e678
KimHyunJin <khj745700@naver.com>
2023-10-28 20:57:57 +0900

    UserMapper 작성

---
+++ src/main/java/com/liveshop/application/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.liveshop.application.mapper;
+
+import com.liveshop.application.domain.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.data.repository.query.Param;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("INSERT INTO users (username, password, nickname, phone_num) VALUES (#{username}, #{password}, #{nickname}, #{phoneNum}) ")
+    void insertUser(User user);
+
+    @Select("SELECT id, password, phoneNum, username, nickname FROM users WHERE id = #{id}")
+    User findById(@Param("id") Integer id);
+
+    @Select("SELECT EXISTS(SELECT 1 FROM users WHERE username = #{username}) as exist")
+    Boolean existByUsername(@Param("username") String username);
+
+    @Select("SELECT EXISTS(SELECT 1 FROM users WHERE nickname = #{nickname}) as exist")
+    Boolean existByNickname(@Param("nickname") String nickname);
+}
