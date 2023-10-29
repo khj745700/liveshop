1d670c9e46b07d23358927aca6c4be9d462efec1
KimHyunJin <khj745700@naver.com>
2023-10-29 20:38:24 +0900

    Spring Handler List 추출 https://codingmylife.tistory.com/40

---
+++ src/main/java/com/liveshop/application/utils/HandlerUrlResources.java
@@ -0,0 +1,47 @@
+package com.liveshop.application.utils;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.*;
+
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class HandlerUrlResources {
+
+    private final RequestMappingHandlerMapping requestMappingHandlerMapping;
+    private Set<String> restAPIResources;
+
+    @Bean(name = "restAPIResources")
+    public Set<String> getSet() {
+        if(restAPIResources != null){
+            return restAPIResources;
+        }
+        restAPIResources = new HashSet<>();
+
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> requestMappingInfoHandlerMethodEntry : handlerMethods.entrySet()) {
+            String base = requestMappingInfoHandlerMethodEntry.getKey().toString();
+            log.info(base);
+            String baseUrl = requestMappingInfoHandlerMethodEntry.getKey().getActivePatternsCondition().toString();
+             List<String> resources = Arrays.stream(baseUrl.substring(2, baseUrl.length() - 1)
+                     .split("/"))
+                     .filter(s -> !s.contains("{") || !s.contains("}"))
+                     .toList();
+             restAPIResources.addAll(resources);
+        }
+
+        log.debug(String.valueOf(restAPIResources));
+
+        restAPIResources = Collections.unmodifiableSet(restAPIResources);
+
+        return restAPIResources;
+    }
+}
